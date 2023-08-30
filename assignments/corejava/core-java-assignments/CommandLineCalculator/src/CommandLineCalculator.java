587055b89254e9c5a6b8221b6edd74be5ce4cd87
kajamal <kajasharif9894@gmail.com>
2023-08-30 12:49:52 +0530

    argscalculator

---
+++ assignments/corejava/core-java-assignments/CommandLineCalculator/src/CommandLineCalculator.java
@@ -0,0 +1,27 @@
+public class CommandLineCalculator {
+    public static void main(String[] args) {
+
+        int firstnumber = Integer.valueOf(args[0]);
+        char operator = args[1].charAt(0);
+        int secondnumber = Integer.valueOf(args[2]);
+
+        switch (operator) {
+            case '+':
+                System.out.println("The final result: " + (firstnumber + secondnumber));
+                break;
+            case '-':
+                System.out.println("The final result: " + (firstnumber - secondnumber));
+                break;
+            case '*':
+                System.out.println("The final result: " + (firstnumber * secondnumber));
+                break;
+            case '/':
+                System.out.println("The final result: " + (firstnumber / secondnumber));
+                break;
+            default:
+                System.out.println("invalid operator");
+
+
+        }
+    }
+}
